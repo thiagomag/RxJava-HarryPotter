c47432d168603e9db724a86384ddaaad6fdb1ba3
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2021-09-04 21:42:01 -0300

    update in IdDaCasaNaoEncontradaException.java

--- src/main/java/br/com/thiago/rxjavaharrypotter/exception/IdDaCasaNaoEncontradaException.java
+++ src/main/java/br/com/thiago/rxjavaharrypotter/exception/IdDaCasaNaoEncontradaException.java
@@ -1,5 +1,9 @@
 package br.com.thiago.rxjavaharrypotter.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class IdDaCasaNaoEncontradaException extends RuntimeException {
     public IdDaCasaNaoEncontradaException(String idCasa) {
         super("O id da casa pesquisada, " + idCasa + ", não existe no banco de dados.");
